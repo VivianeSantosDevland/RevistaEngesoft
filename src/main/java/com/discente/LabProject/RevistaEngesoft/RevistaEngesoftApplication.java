74b1a0364da7e160b9a3a08fba1fb275d4b699c3
aluno <aluno@INTRAUECA.CEFET.COM>
2022-05-06 16:56:31 -0300

    Initial commit

---
+++ src/main/java/com/discente/LabProject/RevistaEngesoft/RevistaEngesoftApplication.java
@@ -0,0 +1,13 @@
+package com.discente.LabProject.RevistaEngesoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevistaEngesoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevistaEngesoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/discente/LabProject/RevistaEngesoft/RevistaEngesoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.discente.LabProject.RevistaEngesoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevistaEngesoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
